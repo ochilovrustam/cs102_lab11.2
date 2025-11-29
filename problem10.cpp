658ecd7b462838d8101ba4bc3a684b410cf92987
Rustam Ochilov <r.ochilov@newuu.uz>
2025-11-30 00:08:45 +0500

    problem10.cpp

--- problem10.cpp
+++ problem10.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 using namespace std;
 
+void avg(const double* arr, int size, double* result) {
+    double sum = 0;
+    for (const double* p = arr; p < arr + size; p++)
+        sum += *p;
+    *result = sum / size;
+}
 
 int main() {
+    int n;
+    cin >> n;
+    double arr[100];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
 
+    double result;
+    avg(arr, n, &result);
+
+    cout << result;
+    
 }
